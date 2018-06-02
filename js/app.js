dd55397f98437d0bfb43431474b0396f9af5accf
anthonybronkema <anthony.bronkema@gmail.com>
2018-06-02 13:47:23 -0400

    feat: add ability to click card and show icon

--- js/app.js
+++ js/app.js
@@ -36,3 +36,11 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ const cardDeck = document.querySelectorAll('.card');
+
+ cardDeck.forEach(function(card) {
+    card.addEventListener('click', function() {
+        card.classList.add('open', 'show');
+    });
+ });
